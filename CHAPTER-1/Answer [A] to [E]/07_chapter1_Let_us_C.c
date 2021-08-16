42b3529911ec9511475a3e6030e66b3f83f69b56
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2021-08-16 14:36:15 +0530

    Add files via upload

---
+++ CHAPTER-1/Answer [A] to [E]/07_chapter1_Let_us_C.c
@@ -0,0 +1,59 @@
+/*________________ ANSWER [A]
+
+ '3.15','eLearning','Quest' 
+ Invalid because single quotes are used to take only single character as input
+
+ "35,500","2^3","4 6 5 2" 
+  Invalid because comma and space is not allowed and we cant use exponent in declaration
+
+___________________ANSWER[B]
+
+b'day, #HASH, total%, _main(), %name%, stack-queue, dot.   
+Invalid because special symbol except _ is not allowed in C Language
+
+variable name,1st 
+Invalid because space and varible start with number is not allowed (alphabet or underscore is allowed)
+
+_____________________ANS[C]
+
+(a) True
+(b) True
+(c) True
+(d) True
+(e) True
+(f) True
+(g) True
+(h) True
+(i) True
+(j) False
+(k) True
+(l) False
+
+_________________________ANS[D]
+
+(a) \n        Escape sequences
+(b) 3.145     Real constant
+(c) -6513     Integer constant
+(d) 'D'       Character constant
+(e) 4.25e^-3  Exponential form
+(f) main()    function
+(g) %f,%d,%c  Format specifier
+(h) ;         Statement terminator
+(i) Constant  literal
+(j) Variable  Identifier
+(k) &         Adress of operator
+(l) printf()  Output function
+(m) scanf()   Input function
+
+________________ANS[E]
+
+(a)  No error
+(b)  No error
+(c)  & adress of operator is missing
+(d)  ; semicolon is missing
+
+
+
+
+
+*/
